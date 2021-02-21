1016d7ddc51d2f5fd00945139156e65533809f8c
Zefugi <trowi@live.dk>
2021-02-21 14:57:30 +0100

    ----- Prepared to refactor Perceptron into DeepNet

---
+++ Perceptomancer/Perceptomancer.Tests/DeepNet_Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptomancer.Tests
+{
+    [TestFixture]
+    public class DeepNet_Tests
+    {
+        [Test]
+        public void Ctor_DoesNotFail()
+        {
+            var net = new DeepNet();
+        }
+    }
+}
--- Perceptomancer/Perceptomancer/Perceptron.cs
+++ Perceptomancer/Perceptomancer/Perceptron.cs
@@ -10,28 +10,30 @@ namespace Perceptomancer
     [Serializable]
     public class Perceptron
     {
-        List<Layer> layers;
+        List<Layer> _layers;
 
         public Perceptron(int[] neuronsPerlayer, int randomizationSeed = 1)
         {
-            layers = new List<Layer>();
+            _layers = new List<Layer>();
             Random r = new Random(randomizationSeed);
 
             for (int i = 0; i < neuronsPerlayer.Length; i++)
             {
-                layers.Add(new Layer(neuronsPerlayer[i], i == 0 ? neuronsPerlayer[i] : neuronsPerlayer[i - 1], r));
+                _layers.Add(new Layer(neuronsPerlayer[i], i == 0 ? neuronsPerlayer[i] : neuronsPerlayer[i - 1], r));
             }
         }
+
         public double[] Activate(double[] inputs)
         {
             double[] outputs = new double[0];
-            for (int i = 1; i < layers.Count; i++)
+            for (int i = 1; i < _layers.Count; i++)
             {
-                outputs = layers[i].Activate(inputs);
+                outputs = _layers[i].Activate(inputs);
                 inputs = outputs;
             }
             return outputs;
         }
+
         double IndividualError(double[] realOutput, double[] desiredOutput)
         {
             double err = 0;
@@ -41,6 +43,7 @@ namespace Perceptomancer
             }
             return err;
         }
+
         double GeneralError(List<double[]> input, List<double[]> desiredOutput)
         {
             double err = 0;
@@ -50,6 +53,7 @@ namespace Perceptomancer
             }
             return err;
         }
+
         List<string> log;
         public bool Learn(List<double[]> input, List<double[]> desiredOutput, double alpha, double maxError, int maxIterations, String net_path = null, int iter_save = 1)
         {
@@ -94,7 +98,7 @@ namespace Perceptomancer
                     return false;
                 }
 
-                if(err <= maxError)
+                if (err <= maxError)
                 {
                     Debug.WriteLine(err + " iterations: " + (it - maxIterations));
                     return true;
@@ -112,75 +116,80 @@ namespace Perceptomancer
         void SetSigmas(double[] desiredOutput)
         {
             sigmas = new List<double[]>();
-            for (int i = 0; i < layers.Count; i++)
+            for (int i = 0; i < _layers.Count; i++)
             {
-                sigmas.Add(new double[layers[i].numberOfNeurons]);
+                sigmas.Add(new double[_layers[i].numberOfNeurons]);
             }
-            for (int i = layers.Count - 1; i >= 0; i--)
+            for (int i = _layers.Count - 1; i >= 0; i--)
             {
-                for (int j = 0; j < layers[i].numberOfNeurons; j++)
+                for (int j = 0; j < _layers[i].numberOfNeurons; j++)
                 {
-                    if (i == layers.Count - 1)
+                    if (i == _layers.Count - 1)
                     {
-                        double y = layers[i].neurons[j].lastActivation;
+                        double y = _layers[i].neurons[j].lastActivation;
                         sigmas[i][j] = (Neuron.Sigmoid(y) - desiredOutput[j]) * Neuron.SigmoidDerivated(y);
                     }
                     else
                     {
                         double sum = 0;
-                        for (int k = 0; k < layers[i + 1].numberOfNeurons; k++)
+                        for (int k = 0; k < _layers[i + 1].numberOfNeurons; k++)
                         {
-                            sum += layers[i + 1].neurons[k].weights[j] * sigmas[i + 1][k];
+                            sum += _layers[i + 1].neurons[k].weights[j] * sigmas[i + 1][k];
                         }
-                        sigmas[i][j] = Neuron.SigmoidDerivated(layers[i].neurons[j].lastActivation) * sum;
+                        sigmas[i][j] = Neuron.SigmoidDerivated(_layers[i].neurons[j].lastActivation) * sum;
                     }
                 }
             }
         }
+
         void SetDeltas()
         {
             deltas = new List<double[,]>();
-            for (int i = 0; i < layers.Count; i++)
+            for (int i = 0; i < _layers.Count; i++)
             {
-                deltas.Add(new double[layers[i].numberOfNeurons, layers[i].neurons[0].weights.Length]);
+                deltas.Add(new double[_layers[i].numberOfNeurons, _layers[i].neurons[0].weights.Length]);
             }
         }
+
         void AddDelta()
         {
-            for (int i = 1; i < layers.Count; i++)
+            for (int i = 1; i < _layers.Count; i++)
             {
-                for (int j = 0; j < layers[i].numberOfNeurons; j++)
+                for (int j = 0; j < _layers[i].numberOfNeurons; j++)
                 {
-                    for (int k = 0; k < layers[i].neurons[j].weights.Length; k++)
+                    for (int k = 0; k < _layers[i].neurons[j].weights.Length; k++)
                     {
-                        deltas[i][j, k] += sigmas[i][j] * Neuron.Sigmoid(layers[i - 1].neurons[k].lastActivation);
+                        deltas[i][j, k] += sigmas[i][j] * Neuron.Sigmoid(_layers[i - 1].neurons[k].lastActivation);
                     }
                 }
             }
         }
+
         void UpdateBias(double alpha)
         {
-            for (int i = 0; i < layers.Count; i++)
+            for (int i = 0; i < _layers.Count; i++)
             {
-                for (int j = 0; j < layers[i].numberOfNeurons; j++)
+                for (int j = 0; j < _layers[i].numberOfNeurons; j++)
                 {
-                    layers[i].neurons[j].bias -= alpha * sigmas[i][j];
+                    _layers[i].neurons[j].bias -= alpha * sigmas[i][j];
                 }
             }
         }
+
         void UpdateWeights(double alpha)
         {
-            for (int i = 0; i < layers.Count; i++)
+            for (int i = 0; i < _layers.Count; i++)
             {
-                for (int j = 0; j < layers[i].numberOfNeurons; j++)
+                for (int j = 0; j < _layers[i].numberOfNeurons; j++)
                 {
-                    for (int k = 0; k < layers[i].neurons[j].weights.Length; k++)
+                    for (int k = 0; k < _layers[i].neurons[j].weights.Length; k++)
                     {
-                        layers[i].neurons[j].weights[k] -= alpha * deltas[i][j, k];
+                        _layers[i].neurons[j].weights[k] -= alpha * deltas[i][j, k];
                     }
                 }
             }
         }
+
         void ApplyBackPropagation(List<double[]> input, List<double[]> desiredOutput, double alpha)
         {
             SetDeltas();
@@ -286,6 +295,7 @@ namespace Perceptomancer
                 weights[i] = 10 * r.NextDouble() - 5;
             }
         }
+
         public double Activate(double[] inputs)
         {
             double activation = bias;
@@ -298,10 +308,12 @@ namespace Perceptomancer
             lastActivation = activation;
             return Sigmoid(activation);
         }
+
         public static double Sigmoid(double input)
         {
             return 1 / (1 + Math.Exp(-input));
         }
+
         public static double SigmoidDerivated(double input)
         {
             double y = Sigmoid(input);
